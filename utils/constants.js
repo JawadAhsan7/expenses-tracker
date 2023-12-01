476d5d08ccf1c9791bb66edc7e2d6f67ba53cd53
Jawad Ahsan <jahsandev@gmail.com &&>
2023-12-01 20:43:30 +0500

    add constats.js file for USER_ROLES and EXPENSE_CATEGORIES enums

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+export const USER_ROLES = {
+  ADMIN: 'admin',
+  USER: 'user',
+};
+
+export const EXPENSE_CATEGORIES = {
+  FOOD: 'food',
+  HEALTHCARE: 'healthcare',
+  TRANSPORTATION: 'transportation',
+  TUITION: 'tuition',
+  UTILITIES: 'utilities',
+  ENTERTAINMENT: 'entertainment',
+  CLOTHING: 'clothing',
+  MISCELLANEOUS: 'miscellaneous'
+};
