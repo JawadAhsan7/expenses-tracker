6fe7e10e571b7a438d21fef41fbcc9b9e9cba031
Jawad Ahsan <jahsandev@gmail.com &&>
2023-12-02 17:27:31 +0500

    create and send cookie when signed in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
 import { comparePasswordHash, hashPassword } from '../utils/hashPassword.js';
+import { createJWT } from '../utils/webtoken.js';
 
 export const register = async (req, res) => {
   let { firstName, lastName, email, password } = req.body;
@@ -28,6 +29,17 @@ export const login = async (req, res) => {
   if (!isValidUser) {
     return res.status(StatusCodes.UNAUTHORIZED).json({ msg: 'invalid credentials' });
   }
+
+  const token = createJWT({
+    userId: user._id,
+    role: user.role
+  });
+
+  res.cookie('jwtToken', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 86400000),
+    secure: process.env.NODE_ENV === 'production'
+  });
 
   return res.status(StatusCodes.OK).json({ msg: 'user logged in' });
 };
