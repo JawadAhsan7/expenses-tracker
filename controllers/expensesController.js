5158819957bce5491d08baa6d47055d101602b1a
Jawad Ahsan <jahsandev@gmail.com &&>
2023-12-04 19:18:45 +0500

    create expense

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -1,19 +1,24 @@
-export const createExpense = (req, res) => {
-  res.send('createExpense Controller');
+import { StatusCodes } from 'http-status-codes';
+import Expense from '../models/Expense.js';
+
+export const createExpense = async (req, res) => {
+  const { name, date, amount, category } = req.body;
+  const createdExpense = await Expense.create({ name, date, amount, category });
+  res.status(StatusCodes.CREATED).json({ msg: 'expense created', createdExpense });
 };
 
-export const getAllExpenses = (req, res) => {
-  res.send('getAllExpenses Controller')
+export const getAllExpenses = async (req, res) => {
+  res.send('getAllExpenses Controller');
 };
 
-export const getSingleExpense = (req, res) => {
-  res.send('getSingleExpense Controller')
+export const getSingleExpense = async (req, res) => {
+  res.send('getSingleExpense Controller');
 };
 
-export const updateExpense = (req, res) => {
+export const updateExpense = async (req, res) => {
   res.send('updateExpense Controller');
 };
 
-export const deleteExpense = (req, res) => {
-  res.send('deleteExpense Controller')
-}
+export const deleteExpense = async (req, res) => {
+  res.send('deleteExpense Controller');
+};
