e4f3a4fe1969643f30fd097fdc8069078b5d9efc
Jawad Ahsan <jahsandev@gmail.com &&>
2023-12-04 18:23:07 +0500

    validate register route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,10 +3,11 @@ import { Router } from 'express';
 
 // local import
 import { register, login } from '../controllers/authController.js';
+import { validateRegisterUserInput } from '../middlewares/validationMiddleware.js';
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register', validateRegisterUserInput, register);
 router.post('/login', login);
 
 export default router;
