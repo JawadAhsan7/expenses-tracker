87a6d327470d7b3929592c158eba8c1d2970fe8a
Jawad Ahsan <jahsandev@gmail.com &&>
2023-12-08 19:05:26 +0500

    setup error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -9,7 +9,9 @@ const ErrorPage = () => {
     return (
       <Wrapper>
         <div className="content-container">
-          <h1>Not found</h1>
+          <h1>404</h1>
+          <p>The page you&#39;re looking for is not available.</p>
+          <p>Go to <Link to='/'>Homepage</Link></p>
         </div>
       </Wrapper>
     );
@@ -18,7 +20,9 @@ const ErrorPage = () => {
   return (
     <Wrapper>
       <div className="content-container">
-        <h1>Something went wrong</h1>
+        <h1>Error</h1>
+        <p>Something went wrong, please try later</p>
+        <p>Go to <Link to='/'>Homepage</Link></p>
       </div>
     </Wrapper>
   );
@@ -26,7 +30,28 @@ const ErrorPage = () => {
 export default ErrorPage;
 
 const Wrapper = styled.main`
+  /* 
+  SPACING SYSTEM (px)
+  2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128 / 144 / 160
+
+  FONT SIZE SYSTEM (px)
+  10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98 / 112 / 128 / 144 / 160
+  */
   display: flex;
   justify-content: center;
   align-items: center;
+  min-height: 100vh;
+  text-align: center;
+
+  h1 {
+    font-size: 16rem;
+    font-weight: 300;
+    margin: 0;
+    margin-bottom: 3rem;
+  }
+
+  p {
+    font-size: 1.8rem;
+    margin: 0.2rem;
+  }
 `; 
